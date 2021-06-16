e2f898335e3fa5a4e028f6d17386a34a3595ad7d
RAGHAVAN <RAGHAVAN@microsoft.com>
2021-06-16 20:42:38 +0530

    Travis config

---
+++ config/dev.js
@@ -0,0 +1,11 @@
+// Don't be like me and commit this file!
+// These keys have been disabled, but remain here for reference purposes.
+module.exports = {
+  googleClientID:
+    '577129237751-a12d41r0c7moj03ukru4gs0603mtcoj8.apps.googleusercontent.com',
+  googleClientSecret: 'IE4qvOzcljxwpKei7CJJBTvj',
+  mongoURI: 'mongodb://127.0.0.1:27017/advance',
+  cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379'
+};
+
